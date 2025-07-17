f2391d7ea971f0a3bb8cf63230a9604a1a2a88b6
ArcGabo <pcjhonny13@gmail.com>
2025-07-17 05:01:13 -0500

    CHALE YA LA KGUE

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnh8ExE_le-GaOw3gkooNKLqryBF5ItNY",
-  authDomain: "sistema-crm-compina.firebaseapp.com",
-  projectId: "sistema-crm-compina",
-  storageBucket: "sistema-crm-compina.firebasestorage.app",
-  messagingSenderId: "916956050513",
-  appId: "1:916956050513:web:de337b89798650594bf470"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
