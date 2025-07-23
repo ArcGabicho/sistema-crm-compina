368cd54bafa04236333291fe7ed3c51706e5d9a6
ArcGabo <pcjhonny13@gmail.com>
2025-07-23 07:38:50 -0500

    Agregar Metadatos Pro

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import { Toaster } from "react-hot-toast";
+// app/layout.tsx
 import { Poppins } from "next/font/google";
-import Head from "next/head";
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -10,35 +10,51 @@ const poppins = Poppins({
   display: "swap",
 });
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+  title: "Sistema CRM | Compina S.A.C.",
+  description:
+    "Sistema CRM para gestión de clientes de Compina con capacidad de gestión de 6000 clientes en tiempo real.",
+  authors: [{ name: "Gabriel Polack" }],
+  openGraph: {
+    title: "Sistema CRM | Compina S.A.C.",
+    description:
+      "Sistema CRM para gestión de clientes de Compina con capacidad de gestión de 6000 clientes en tiempo real.",
+    url: "https://sistema-crm-compina.vercel.app/",
+    siteName: "Sistema CRM | Compina",
+    images: [
+      {
+        url: "https://i.imgur.com/RYfsVS9.png",
+        width: 1200,
+        height: 630,
+        alt: "Sistema CRM",
+      },
+    ],
+    locale: "es_PE",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sistema CRM | Compina S.A.C.",
+    description:
+      "Sistema CRM para gestión de clientes de Compina con capacidad de gestión de 6000 clientes en tiempo real.",
+    images: ["https://i.imgur.com/RYfsVS9.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <>
-      <html lang="es" className={`${poppins.variable}`}>
-        <Head>
-          <meta name="viewport" content="width=device-width" />
-          <meta name="title" content="Sistema CRM | Compipro S.A.C." />
-          <meta name="description" content="Sistema CRM para gestión de clientes de Compina con capacidad de gestión de 6000 clientes en tiempo real." />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="author" content="Gabriel Polack"/>
-          <meta name="image" content="https://i.imgur.com/RYfsVS9.png" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://astro-compipro-page.pages.dev/" />
-          <meta property="og:title" content="Sistema CRM | Compipro S.A.C." />
-          <meta property="og:description" content="Sistema CRM para gestión de clientes de Compina con capacidad de gestión de 6000 clientes en tiempo real."/>
-          <meta property="og:image" content="https://i.imgur.com/RYfsVS9.png"/>
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://astro-compipro-page.pages.dev/"/>
-          <meta property="twitter:title" content="Sistema CRM | Compipro S.A.C." />
-          <meta property="twitter:description" content="Sistema CRM para gestión de clientes de Compina con capacidad de gestión de 6000 clientes en tiempo real."/>
-          <meta property="twitter:image" content="https://i.imgur.com/RYfsVS9.png"/>
-          <link rel="icon" type="image/svg+xml" href="/logo-compipro.png" />
-          <title>Sistema CRM | Compina S.A.C.</title>
-        </Head>
-        <body>
-          {children}
-           <Toaster position="top-right" />
-        </body>
-      </html>
-    </>
+    <html lang="es" className={poppins.variable}>
+      <body>
+        {children}
+        <Toaster position="top-right" />
+      </body>
+    </html>
   );
 }
